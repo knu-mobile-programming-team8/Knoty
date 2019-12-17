21280e80048abc10a5eee0e0f92b22a95a3bb6ea
툰라온 <hmt7136@gmail.com>
2019-12-18 01:17:56 +0900

    옵션 버튼 생성 (기능은 없음)

--- app/src/main/java/com/example/knoty/MainActivity.java
+++ app/src/main/java/com/example/knoty/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.knoty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //옵션 메뉴 등록
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    //옵션 메뉴 선택시 핸들러
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menu1: //옵션 메뉴
+                Toast.makeText(this, "option selected", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
