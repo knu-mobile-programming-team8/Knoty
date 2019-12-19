5ebf302b1e5f900d8530da4287de8fd949ece5d9
exqt <exqt_@naver.com>
2019-12-19 20:05:41 +0900

    레코드 지울 수 있도록 & 약간의 주석

--- app/src/main/java/com/example/knoty/AnnouncementScraper.java
+++ app/src/main/java/com/example/knoty/AnnouncementScraper.java
@@ -37,11 +37,13 @@ public abstract class AnnouncementScraper {
         this.ctx = ctx;
     }
 
+    // 백그라운드 작업 실행
     public void doScrapTask(int category, int page) {
         ScrapTask scrapTask = new ScrapTask();
         scrapTask.execute(category, page);
     }
 
+    // DB에 레코드 업데이트 // 없으면 추가되고 있으면 변경 없음
     private void updateListInStorage(List<Announcement> list) {
         AnnouncementDBHelper DBHelper = new AnnouncementDBHelper(ctx);
         SQLiteDatabase DB = DBHelper.getWritableDatabase();
@@ -65,6 +67,7 @@ public abstract class AnnouncementScraper {
         }
     }
 
+    // DB에 있는 레코드들을 반환
     public ArrayList<Announcement> getListInStorage(int category, int maxItems, boolean onlyUnpushed) {
         ArrayList<Announcement> list = new ArrayList<Announcement>();
 
@@ -131,6 +134,28 @@ public abstract class AnnouncementScraper {
         ArrayList<Announcement> list = new ArrayList<Announcement>();
         list.add(a);
         markAsPushed(list);
+    }
+
+    // DB에 있는 레코드 지움
+    public void deleteRecords(List<Announcement> list) {
+        AnnouncementDBHelper DBHelper = new AnnouncementDBHelper(ctx);
+        SQLiteDatabase DB = DBHelper.getWritableDatabase();
+
+        for(Announcement a : list) {
+            DB.delete(
+                "announcement",
+                "id = ? AND category = ? AND num = ?",
+                new String[]{ Integer.toString(a.id), Integer.toString(a.category), Integer.toString(a.num) }
+            );
+        }
+
+        DB.close();
+    }
+
+    public void deleteRecord(Announcement a) {
+        ArrayList<Announcement> list = new ArrayList<Announcement>();
+        list.add(a);
+        deleteRecords(list);
     }
 
     public abstract ArrayList<Announcement> scrap(int category, int page) throws IOException;
